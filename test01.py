c4831eb5310a273ae28c6937ac0b28af3a7d8691
messilin <3524255030@qq.com>
2024-08-15 14:33:26 +0800

    UI自动化代码

---
+++ test01.py
@@ -0,0 +1,10 @@
+# @项目名  :  toast
+# @模块名  :  test01.py
+# @author :  袁某人
+# @time   :  2024/08/15 14:18
+class TestDemo:
+    def test_01(self):
+        assert 1 == 1
+
+    def test_02(self):
+        assert 1 == 2
